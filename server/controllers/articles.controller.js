903f880adeb64b5a6222de7db0d160fda5557644
MonMaramba <monmaramba@yahoo.com>
2022-09-26 12:32:05 -0700

    added delete and retrieve all articles functionalities

--- server/controllers/articles.controller.js
+++ server/controllers/articles.controller.js
@@ -30,6 +30,44 @@ const articlesController = {
       next(error);
     }
   },
+
+  async updateArticleById(req, res, next) {
+    try {
+      const _id = req.params.id;
+      const article = await articlesService.updateArticleById(_id, req.body);
+      res.json(article);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async deleteArticleById(req, res, next) {
+    try {
+      const _id = req.params.id;
+      await articlesService.deleteArticleById(_id);
+      res.status(httpStatus.OK).json({ action: 'deleted' });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async getAllArticles(req, res, next) {
+    try {
+      const articles = await articlesService.allArticles(req);
+      res.json(articles);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async getMoreArticles(req, res, next) {
+    try {
+      const articles = await articlesService.moreArticles(req);
+      res.json(articles);
+    } catch (error) {
+      next(error);
+    }
+  },
 };
 
 module.exports = articlesController;
