77608684a1a50cf21a490aee7538c081587aa57d
MonMaramba <monmaramba@yahoo.com>
2022-09-27 12:42:30 -0700

    Routes, header components and home page created. Bootstrap fredoka font not working yet

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import Routes from './routes';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/main.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Routes />);
